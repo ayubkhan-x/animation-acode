f7341f0743cca115474ba86a271a66a7ece52fe4
ayubkhan-dev <ayubkhan.khakimov@mail.ru>
2022-12-02 22:51:06 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let l =  document.querySelector('.l-v-mode-t')
+let d =  document.querySelector('.d-v-mode-t')
+
+let body =  document.getElementsByTagName('body')
+
+
+d.addEventListener('click', ()=> {
+	console.log('adjkfdj');
+})
